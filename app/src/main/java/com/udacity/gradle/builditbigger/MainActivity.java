d69780a29fb58226a8ae05da5a3df8d63c03369d
aman-alam11 <aman.alam11@yahoo.com>
2018-08-05 04:11:04 -0400

    connected backend via async task code

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,22 +1,42 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
+    ProgressBar mProgressBar;
+    FrameLayout mRootFragmentHolder;
+    private static MyApi myApiService = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mProgressBar = findViewById(R.id.loading_pb);
+        mRootFragmentHolder = findViewById(R.id.fragment);
+
         MainActivityFragment mainActivityFragment = new MainActivityFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.fragment, mainActivityFragment).commit();
     }
@@ -45,8 +65,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, getResources().getString(R.string.app_name), Toast.LENGTH_SHORT).show();
+        mProgressBar.setVisibility(View.VISIBLE);
+        mRootFragmentHolder.setAlpha((float) 0.4);
+        new EndpointsAsyncTask().execute(this);
     }
 
 
+    /**
+     * Async task to get jokes
+     */
+    class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
+
+
+        @Override
+        protected String doInBackground(Context... params) {
+            if (myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest)
+                                    throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            Context context = params[0];
+
+            try {
+
+                return myApiService.getJokesMethod().execute().getData();
+            } catch (IOException e) {
+                Log.e("Error", e.getMessage());
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            // Pass Data for display
+            mProgressBar.setVisibility(View.INVISIBLE);
+            mRootFragmentHolder.setAlpha(1);
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+        }
+    }
 }
+
